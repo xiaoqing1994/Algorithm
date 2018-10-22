03c8ddb954648d491d1c1e8463024df94113c56b
Huang <xh4715@ic.ac.uk>
2018-10-22 15:45:54 +0100

    target sum

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"The algorithm"
+```c++
+int s = 
+```
---
+++ target_sum.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<limits>
+#include<cmath>
+using namespace std;
+
+//----- The first solution used recursion, but time-consuming---
+// class Solution {
+// public:
+//     int findTargetSumWays(vector<int>& nums, int S) {
+//         int ways =0;
+//         s = S;
+//         vector<int> copy(nums);
+//         int num = copy.front();
+//         ways += DFS(copy, num, 1);
+//         // cout<<"ways = "<<ways<<endl;
+//         ways += DFS(copy, -num, 1);
+//         return ways;
+//     }
+//
+//     int DFS(vector<int> nums, int sum, int ind){
+//         if(ind == nums.size()){
+//             if(sum ==s){
+//                 // cout<<"return 1"<<endl;
+//                 return 1;
+//             }
+//             else return 0;
+//         }
+//         else if(ind < nums.size()){
+//             int num = nums[ind];
+//             int leftdepth = DFS(nums, sum+num, ind+1);
+//             // cout<<sum<<" turn right"<<endl;
+//             int rightdepth = DFS(nums, sum-num, ind+1);
+//
+//             return leftdepth + rightdepth ;
+//         }
+//     }
+// private:
+//     int s;
+// };
+
+int main(){
+  vector<int> nums{33,36,38,40,25,49,1,8,50,13,41,50,29,27,18,25,37,8,0,48};
+  int s = 0;
+  Solution so;
+  int result = so.findTargetSumWays(nums,s);
+  cout<<"The result is "<<result<<endl;
+  return 0;
+  system("pause");
+}
